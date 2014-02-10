1ca563a7eff6798b5215f877c802fe05f7d096da
Jens Anders Bakke <jens.anders.bakke@gmail.com>
2014-02-10 18:33:59 +0100

    setup done

---
+++ test/tests.js
@@ -0,0 +1,13 @@
+// Ractive-decorators-minmaxwidth tests
+// ==============================
+
+(function () {
+
+	var fixture;
+
+	test( 'We are ready to write some tests', function ( t ) {
+		t.ok( true );
+	});
+
+
+}());
