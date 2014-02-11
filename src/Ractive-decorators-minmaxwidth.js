9bbc4d650714524a3a74b07460dcda8ba478fecb
Jens Anders Bakke <jens.anders.bakke@gmail.com>
2014-02-11 11:53:53 +0100

    Ractive allready has getComputedStyle polyfill

--- src/Ractive-decorators-minmaxwidth.js
+++ src/Ractive-decorators-minmaxwidth.js
@@ -59,22 +59,6 @@
 	'use strict';
 
     // IE8 polyfills
-    if (!window.getComputedStyle) {
-        window.getComputedStyle = function(el) {
-            this.el = el;
-            this.getPropertyValue = function(prop) {
-                var re = /(\-([a-z]){1})/g;
-                if (prop == 'float') prop = 'styleFloat';
-                if (re.test(prop)) {
-                    prop = prop.replace(re, function () {
-                        return arguments[2].toUpperCase();
-                    });
-                }
-                return el.currentStyle[prop] ? el.currentStyle[prop] : null;
-            };
-            return this;
-        };
-    }
     if(!Array.isArray) {
         Array.isArray = function (vArg) {
             var isArray;
